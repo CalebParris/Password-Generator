68326deb9810292261a6dbb1bddebc944e57bb12
Caleb Parris <calebparris@live.com>
2020-07-25 18:09:53 -0500

    Added propmts and confirms

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+  var length = prompt("How many characters would you like your password to contain?");
+  var special = confirm("Click OK to confirm including special characters.");
+  var numbers = confirm("Click OK to confirm including numeric characters.");
+  var lowerCase = confirm("Click OK to confirm including lowercase characters.");
+  var upperCase = confirm("Click ok to confirm including uppercase characters.");
+  
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
