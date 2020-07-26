fca46ba954d9aef278a8ee527fbb9bdea22be34a
Caleb Parris <calebparris@live.com>
2020-07-26 14:50:20 -0500

    Added object for password values

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Added an object for the password values
+var passwordOptions = {
+  special: "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+  numbers: "0123456789",
+  lowerCase: "acbdefghijklmnopqrstuvwxyz",
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,12 +19,12 @@ function writePassword() {
 }
 
 function generatePassword(){
-  var length = prompt("How many characters would you like your password to contain?");
-  var special = confirm("Click OK to confirm including special characters.");
-  var numbers = confirm("Click OK to confirm including numeric characters.");
-  var lowerCase = confirm("Click OK to confirm including lowercase characters.");
-  var upperCase = confirm("Click ok to confirm including uppercase characters.");
-  
+  // Declared the prompts and confirms for the password choices
+  var lengthInput = prompt("How many characters would you like your password to contain?");
+  var specialInput = confirm("Click OK to confirm including special characters.");
+  var numbersInput = confirm("Click OK to confirm including numeric characters.");
+  var lowerCaseInput = confirm("Click OK to confirm including lowercase characters.");
+  var upperCaseInput = confirm("Click ok to confirm including uppercase characters.");
 }
 
 // Add event listener to generate button
