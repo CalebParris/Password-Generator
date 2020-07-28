54762c84c02939ba376209eb8da23cc9ee1e3379
Caleb Parris <calebparris@live.com>
2020-07-28 06:25:00 -0500

    Accounted for user error and Updated README

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Password-Generator
 
 ## Objective
-The objective of this homework was to make a working random password generator. To accomplish this, I had to create the code for a function that promted the user on how long they wanted the password to be and if they wanted any special character, numbers, lowercase letter, and/or uppercase letters.
+The objective of this homework was to make a working random password generator. To accomplish this, I had to create the code for a function that promted the user on how long they wanted the password to be and if they wanted any special characters, numbers, lowercase letters, or uppercase letters in their password.
 
 ## Challenges and Solutions
-The biggest challenge I faced while working on this was getting the possible pool of characters to include all of the user choices from the prompts. The solution to this was to create an object that held all of the values for each of the choices. Then once that was complete, I created several if statements that would add the values to the possible pool of characters based on the prompt choices.
+The biggest challenge I faced while working on this was getting the possible pool of characters to include all of the user's choices from the prompts. The solution to this was to create an object that held all of the values for each of the choices. Then once that was complete, I created several if statements that would add the values to the possible pool of characters based on the user's input.
+
+Another challenge I faced was accounting for user error, which includes things like leaving the prompt empty, or typing something other than a letter, or hitting the cancel button. I was able to resolve this with some while loops and if statments. The while loop accounted for any incorrect input whether it was an empty prompt or somthing other than a number. The if statments accounted for if the user no longer wanted to generate a password during the step where the length is chosen.
 
 ## Links and Screenshots
 Here is a link to the complete working generator: https://calebparris.github.io/Password-Generator/ <br/>
--- script.js
+++ script.js
@@ -19,18 +19,45 @@ function writePassword() {
 }
 
 function generatePassword(){
-  // Declared the prompts and confirms for the password choices
-  var lengthInput = prompt("How many characters would you like your password to contain?");
-  var specialInput = confirm("Click OK to confirm including special characters.");
-  var numbersInput = confirm("Click OK to confirm including numeric characters.");
-  var lowerCaseInput = confirm("Click OK to confirm including lowercase characters.");
-  var upperCaseInput = confirm("Click ok to confirm including uppercase characters.");
+  // Prompts you for the length of the password
+  var lengthInput = prompt("Please choose a password containing between 8 and 128 characters in length?");
+
+  // Cancels the promt and refreshes the page if the 'cancel' button is clicked
+  if (lengthInput === null){
+    return location.reload();
+  }
+
+  // Loops continuously until a valid number input is entered between 8 and 128
+  while (lengthInput < 8 || lengthInput > 128 || isNaN(lengthInput)){
+    lengthInput = prompt("Invalid entry, password must be between 8 and 128 characters in length. Please enter a valid length.");
+
+    // Cancels the promt and refreshes the page if the 'cancel' button is clicked
+    if (lengthInput === null){
+      return location.reload();
+    }
+  }
+
+  // Prompts the user to confirm what type of characters they want in the password
+  var specialInput = confirm("Do you want to include special characters?");
+  var numbersInput = confirm("Do you want to include number?");
+  var lowerCaseInput = confirm("Do you want to include lowercase letters?");
+  var upperCaseInput = confirm("Do you want to include uppercase letters?");
+
+  // Loops continuously until at least one of the choices is selected
+  while (specialInput === false && numbersInput === false && lowerCaseInput === false && upperCaseInput === false){
+    alert("You must choose one of the options to continue")
+    var specialInput = confirm("Do you want to include special characters?");
+    var numbersInput = confirm("Do you want to include number?");
+    var lowerCaseInput = confirm("Do you want to include lowercase letters?");
+    var upperCaseInput = confirm("Do you want to include uppercase letters?");
+  }
+
 
   // Defines the variables as a string
   var passwordChars = "";
   var finalPassword = "";
 
-  // Determines what characters are available to choose from in the password
+  // Determines what characters are available in the password based on the user's previous input
   if (specialInput){
     passwordChars += passwordOptions.special;
   } 
@@ -44,10 +71,10 @@ function generatePassword(){
     passwordChars += passwordOptions.upperCase;
   }
   
-  // Loops number of times equal to the input amount
+  // Loops until the user's chosen length is reached
   for (i = 0; i < parseInt(lengthInput); i++){
 
-    // Appends a random character from the available choices to the end of the previous loop
+    // Concatenates a random character from the available choices to the end of the password until it reaches the length that was chosen by the user
     finalPassword += passwordChars.charAt(Math.floor(Math.random() * passwordChars.length));
   }
 
